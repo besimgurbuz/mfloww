49875dd3476d2b840af8f5651ab35c61944f44a8
Besim Gürbüz <mbesimgurbuz@gmail.com>
2022-09-29 22:26:56 +0300

    Initial commit

---
+++ apps/mfloww-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfloww-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mfloww-web';
+}
