c9ee4c9953c07463135bc6e84748612494f3f47b
Besim Gürbüz <mbesimgurbuz@gmail.com>
2022-11-19 13:26:37 +0300

    replace window.crypto with cryto-js

--- libs/mfloww-db/src/lib/services/crypto/crypto-secret.service.ts
+++ libs/mfloww-db/src/lib/services/crypto/crypto-secret.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { lib } from 'crypto-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CryptoSecretService {
-  private CRYPTO_SECRET = window.crypto.randomUUID();
+  private CRYPTO_SECRET = lib.WordArray.random(16).toString();
 
   set secret(secret: string) {
     this.CRYPTO_SECRET = secret;
