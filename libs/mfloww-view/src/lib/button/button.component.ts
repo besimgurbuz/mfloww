56c0d3747b32c74f2ef93d0a1dd22b368afcd39e
Besim Gürbüz <mbesimgurbuz@gmail.com>
2023-01-23 22:13:38 +0300

    style(user): update some paddings

--- libs/mfloww-view/src/lib/button/button.component.ts
+++ libs/mfloww-view/src/lib/button/button.component.ts
@@ -9,7 +9,7 @@ import {
   selector: 'mfloww-view-button',
   template: `
     <button
-      class="hover:opacity-80 flex items-center justify-center w-full h-full p-[2px] active:p-0 focus:p-0 active:border-2 focus:border-2 active:border-solid focus:border-solid focus:border-mfloww_fg active:rounded-full focus:rounded-full"
+      class="hover:opacity-80 flex items-center justify-center w-full h-full active:p-0 focus:p-0 active:border-2 focus:border-2 active:border-solid focus:border-solid focus:border-mfloww_fg active:rounded-full focus:rounded-full"
       (click)="clicked.emit()"
     >
       <ng-content></ng-content>
