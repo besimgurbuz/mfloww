7dfadf1f56729b957c843aa8082ed5c3b10db6a8
Besim Gurbuz <contact@besimgurbuz.dev>
2023-05-22 01:52:15 +0300

    change import location of crypto package in utils.ts

--- apps/mfloww-api/src/app/shared/utils.ts
+++ apps/mfloww-api/src/app/shared/utils.ts
@@ -1,4 +1,4 @@
-import { pbkdf2Sync } from 'node:crypto';
+import { pbkdf2Sync } from 'crypto';
 
 export function hashPassword(password: string, salt: string): string {
   return pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
