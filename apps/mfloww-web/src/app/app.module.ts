9dd54e36eab06b73a9ed7f83505e360033b7d300
Besim Gurbuz <contact@besimgurbuz.dev>
2023-06-11 00:08:05 +0300

    add service worker to mfloww-web

--- apps/mfloww-web/src/app/app.module.ts
+++ apps/mfloww-web/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { MenuComponent } from './components/menu/menu.component';
 import { CoreModule } from './core/core.module';
 import { ProgressTriggererInterceptorService } from './interceptors/progress-triggerer-interceptor.service';
 import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, FooterComponent],
@@ -27,6 +29,12 @@ import { SharedModule } from './shared/shared.module';
     MflowwOverlayPanelComponent,
     MflowwProgressBarComponent,
     MflowwDbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
