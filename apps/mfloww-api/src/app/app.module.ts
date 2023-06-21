a658ef606680201173aee53b591b97b440033966
Besim Gurbuz <contact@besimgurbuz.dev>
2023-06-21 20:02:26 +0300

    update workspace to version 15.9.4

--- apps/mfloww-api/src/app/app.module.ts
+++ apps/mfloww-api/src/app/app.module.ts
@@ -1,30 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AngularUniversalModule } from './angular-universal/angular-universal.module';
 
 import { ScheduleModule } from '@nestjs/schedule';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { ContributorsModule } from './contributors/contributors.module';
 import { PrismaService } from './core/prisma.service';
 import { ExchangeModule } from './exchange/exchange.module';
 import { ReleasesModule } from './releases/releases.module';
 import { UserModule } from './user/user.module';
-import { loadESModule } from './utils/load-esm-module';
 
 @Module({
   imports: [
-    AngularUniversalModule.forRoot(async () => {
-      const angularModule = await loadESModule<{
-        default: typeof import('../../../mfloww-web/src/main.server');
-      }>(join(process.cwd(), 'dist/apps/mfloww-web/server/main.js'));
-
-      return {
-        bootstrap: angularModule.default.AppServerModule,
-        ngExpressEngine: angularModule.default.ngExpressEngine,
-        viewsPath: join(process.cwd(), 'dist/apps/mfloww-web/browser'),
-        cache: true,
-      };
-    }),
     ScheduleModule.forRoot(),
     UserModule,
     AuthModule,
