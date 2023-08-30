459303267913b625fc989b7a2eec3fb1fab25af9
Besim Gurbuz <contact@besimgurbuz.dev>
2023-08-30 03:23:57 +0300

    introduce chart component with echarts

---
+++ apps/mfloww-web/src/app/dashboard/chart/chart.component.ts
@@ -0,0 +1,81 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  effect,
+  signal,
+} from '@angular/core';
+import * as echarts from 'echarts';
+import {
+  ChartSeries,
+  DefaultExpenseSeries,
+  DefaultRevenueSeries,
+} from '../models/chart-series';
+
+@Component({
+  selector: 'mfloww-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chart.component.html',
+})
+export class ChartComponent implements AfterViewInit {
+  @Input() set chartType(chartType: ChartSeries['type']) {
+    this._type = chartType;
+
+    this.revenueSeries.mutate((series) => (series.type = chartType));
+    this.expenseSeries.mutate((series) => (series.type = chartType));
+  }
+  get chartType() {
+    return this._type;
+  }
+  private _type: 'line' | 'bar' = 'line';
+  private revenueSeries = signal<ChartSeries>(DefaultRevenueSeries);
+  private expenseSeries = signal<ChartSeries>(DefaultExpenseSeries);
+
+  @ViewChild('chartContainer', { read: ElementRef })
+  chartContainerElementRef!: ElementRef<HTMLDivElement>;
+
+  private _chartInstance!: echarts.ECharts;
+
+  constructor() {
+    effect(() => {
+      const revenueSeries = this.revenueSeries();
+      const expenseSeries = this.expenseSeries();
+
+      this._chartInstance?.setOption({
+        series: [revenueSeries, expenseSeries],
+      });
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this._chartInstance = echarts.init(
+      this.chartContainerElementRef.nativeElement,
+      'dark'
+    );
+
+    this._chartInstance.setOption({
+      tooltip: {},
+      backgroundColor: 'transparent',
+      textStyle: {
+        fontFamily: 'Krona One',
+      },
+      grid: {
+        x: 0,
+        x2: 0,
+      },
+      xAxis: {
+        data: [],
+      },
+      yAxis: {},
+      series: [],
+    });
+  }
+
+  resizeChart() {
+    this._chartInstance.resize();
+  }
+}
--- apps/mfloww-web/src/app/dashboard/graph/graph.component.ts
+++ apps/mfloww-web/src/app/dashboard/graph/graph.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ChartComponent } from '../chart/chart.component';
 
 @Component({
   selector: 'mfloww-graph',
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.scss'],
   standalone: true,
+  imports: [ChartComponent, ReactiveFormsModule],
 })
-export class GraphComponent {}
+export class GraphComponent {
+  _chartTypeControl = new FormControl<'line' | 'bar'>('line', {
+    nonNullable: true,
+  });
+}
