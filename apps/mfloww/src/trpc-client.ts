23e3e418c3b7bc3ea91c311198031029eecbc06a
Besim Gurbuz <contact@besimgurbuz.dev>
2023-12-02 17:03:01 +0100

    revert change in trpc-client

--- apps/mfloww/src/trpc-client.ts
+++ apps/mfloww/src/trpc-client.ts
@@ -3,7 +3,7 @@ import { inject } from '@angular/core';
 import { AppRouter } from './server/trpc/routers';
 
 export const { provideTrpcClient, TrpcClient } = createTrpcClient<AppRouter>({
-  url: `${import.meta.env['VITE_TRPC_URL']}/api/trpc`,
+  url: '/api/trpc',
 });
 
 export function injectTrpcClient() {
