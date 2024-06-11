e7b451ca66d452905f5df24d9bb1f0b99c228019
Besim Gurbuz <contact@besimgurbuz.dev>
2024-06-11 21:47:56 +0200

    update base structure and introduce closed beta check

--- src/app/dashboard/wrapper.tsx
+++ src/app/dashboard/wrapper.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useContext, useState } from "react"
-import { PlusIcon } from "@radix-ui/react-icons"
+import { CaretLeftIcon, CaretRightIcon, PlusIcon } from "@radix-ui/react-icons"
 
 import { useMediaQuery } from "@/lib/hooks"
 import { useTransactionStatistics } from "@/lib/local-db/hooks"
@@ -21,8 +21,13 @@ import { NoDataCard } from "@/components/no-data-card"
 import { DashboardStateContext } from "@/app/dashboard/dashboard-context"
 
 export function DashboardWrapper() {
-  const { selectedEntry, entryTransactions, entryIncomes, entryExpenses } =
-    useContext(DashboardStateContext)
+  const {
+    selectedEntry,
+    entryTransactions,
+    entryIncomes,
+    entryExpenses,
+    setSelectedEntry,
+  } = useContext(DashboardStateContext)
   const statistics = useTransactionStatistics()
   const isDesktop = useMediaQuery("(min-width: 768px)")
   const [createTransactionOpen, setCreateTransactionOpen] = useState(false)
@@ -33,29 +38,40 @@ export function DashboardWrapper() {
 
   return (
     <>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              onClick={() => setCreateTransactionOpen(true)}
-              className="px-2 h-8"
-            >
-              <PlusIcon className="w-5 h-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent className="flex gap-2" side="bottom">
-            <p>Create transaction</p>
-            <CommandShortcut>⌘K</CommandShortcut>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <CreateUpdateTransaction
-        enableShortcut={true}
-        isDesktop={isDesktop}
-        mode="create"
-        open={createTransactionOpen}
-        onOpenChange={setCreateTransactionOpen}
-      />
+      <div className="flex justify-center md:justify-between ">
+        <div className="flex gap-2 items-center">
+          <Button variant="ghost">
+            <CaretLeftIcon className="w-5 h-5" />
+          </Button>
+          <h2 className="font-medium">{selectedEntry.name}</h2>
+          <Button variant="ghost">
+            <CaretRightIcon className="w-5 h-5" />
+          </Button>
+        </div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                onClick={() => setCreateTransactionOpen(true)}
+                className="px-2 h-16 w-16 fixed bottom-8 right-8 md:h-8 md:w-8 md:relative md:bottom-0 md:right-0"
+              >
+                <PlusIcon className="w-8 h-8" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent className="flex gap-2" side="bottom">
+              <p>Create transaction</p>
+              <CommandShortcut>⌘K</CommandShortcut>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <CreateUpdateTransaction
+          enableShortcut={true}
+          isDesktop={isDesktop}
+          mode="create"
+          open={createTransactionOpen}
+          onOpenChange={setCreateTransactionOpen}
+        />
+      </div>
       <SummaryCards {...statistics} />
       <BalanceCard incomes={entryIncomes} expenses={entryExpenses} />
       <TransactionTableCard
